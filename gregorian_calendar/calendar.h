8c862b8a1cc0f832f6ae7cf6675b4a4be89fba63
MissTipo <dorine.a.tipo@gmail.com>
2024-03-27 12:35:12 +0300

    Define Month struct and function prototypes
    Define the struct Month representing a month with a name and a number of
    days.
    This commit also declares function prototypes for the functions
    implemented in the calendar.c to enable function re-usability if need be.
    
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

---
+++ gregorian_calendar/calendar.h
@@ -0,0 +1,16 @@
+#ifndef CALENDAR_H
+#define CALENDAR_H
+
+#define MONTHS_IN_YEAR 13  // Includes an element for leap year adjustment
+
+typedef struct {
+  char name[20];
+  int days;
+} Month;
+
+int is_leap_year(int year);
+int get_year_start_day(int year);
+void print_calendar(int year, int daycode);
+int enter_year(void);
+
+#endif //CALENDAR_H
